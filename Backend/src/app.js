9126b2579584cb2f79286b6eadd0533a12a61207
Jsbist21 <bishtjatin477@gmail.com>
2024-08-22 17:32:59 +0530

    final commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(
   express.json({
